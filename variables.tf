5bff6d7b6ca21f4b2c471c0b8c3d401bd358f174
hyejeong-jin <wlsgpwjd87@gmail.com>
2024-07-23 19:04:51 +0900

    re add env variable

--- variables.tf
+++ variables.tf
@@ -37,13 +37,13 @@ variable "placeholder" {
   description = "Image-as-a-service URL. Some other fun ones to try are fillmurray.com, placecage.com, placebeard.it, loremflickr.com, baconmockup.com, placeimg.com, placebear.com, placeskull.com, stevensegallery.com, placedog.net"
 }
 
-variable "environmen" {
+variable "environment" {
   type = string
   description = "Define infrastructure's environment"
   default = "dev"
 
   validation {
-    condition = contains(["dev","qa","prod"], var.environmen)
+    condition = contains(["dev","qa","prod"], var.environment)
     error_message = "The environment value must be dev, qa, or prod"
   }
 }
